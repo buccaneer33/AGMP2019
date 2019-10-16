bf13c771002a6f14e5b700a4a3862e4ef531675f
Evgenii Kozlov1 <Evgenii_Kozlov1@epam.com>
2019-10-16 08:49:18 +0400

    [WIP]: added components, work with components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './page-markup/header/header.component';
+import { BreadcrumpsComponent } from './page-markup/breadcrumps/breadcrumps.component';
+import { MainComponent } from './page-markup/main/main.component';
+import { FooterComponent } from './page-markup/footer/footer.component';
+import { SearchBarComponent } from './page-markup/search-bar/search-bar.component';
+import { ItemsListComponent } from './page-markup/items-list/items-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BreadcrumpsComponent,
+    MainComponent,
+    FooterComponent,
+    SearchBarComponent,
+    ItemsListComponent
   ],
   imports: [
     BrowserModule
